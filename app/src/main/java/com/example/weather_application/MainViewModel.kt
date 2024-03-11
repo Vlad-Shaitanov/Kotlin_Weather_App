ac3fe955d27d37833e34d610a3becb2c19505e4a
Vladislav Shaitanov <symphonyofhope@yandex.ru>
2024-03-11 20:39:20 +0300

    Разметка элемента списка. Добавлена модель данный во ViewModel

--- app/src/main/java/com/example/weather_application/MainViewModel.kt
+++ app/src/main/java/com/example/weather_application/MainViewModel.kt
@@ -2,11 +2,12 @@ package com.example.weather_application
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.weather_application.adapters.WeatherModel
 
 class MainViewModel: ViewModel() {
     //Здесь будет прогноз погоды для выбранного времени
-    val liveDataCurrent = MutableLiveData<String>()
+    val liveDataCurrent = MutableLiveData<WeatherModel>()
 
     //Здесь будет прогноз погоды для списка
-    val liveDataList = MutableLiveData<List<String>>()
+    val liveDataList = MutableLiveData<List<WeatherModel>>()
 }
