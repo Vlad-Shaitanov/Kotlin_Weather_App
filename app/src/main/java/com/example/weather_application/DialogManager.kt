bf45dafc053fa1a1b5f763ed89db8fb325d5dccc
Vladislav Shaitanov <symphonyofhope@yandex.ru>
2024-03-16 19:29:08 +0300

    Добавлен диалог с выбором города

--- app/src/main/java/com/example/weather_application/DialogManager.kt
+++ app/src/main/java/com/example/weather_application/DialogManager.kt
@@ -2,6 +2,7 @@ package com.example.weather_application
 
 import android.app.AlertDialog
 import android.content.Context
+import android.widget.EditText
 
 object DialogManager {
     fun locationSettingsDialog(context: Context, listener: Listener) {
@@ -13,7 +14,28 @@ object DialogManager {
         dialog.setMessage("GPS is disabled. Do you want to turn it on?")
 
         dialog.setButton(AlertDialog.BUTTON_POSITIVE, "OK") { _, _ ->
-            listener.onClick()
+            listener.onClick(null)
+            dialog.dismiss()
+        }
+        dialog.setButton(AlertDialog.BUTTON_NEGATIVE, "Cancel") { _, _ ->
+            dialog.dismiss()
+        }
+
+        dialog.show()
+    }
+
+    fun searchByNameDialog(context: Context, listener: Listener) {
+        val builder = AlertDialog.Builder(context)
+        val editText = EditText(context)
+
+        builder.setView(editText) //Добавили в диалог поле с текстом
+
+        //Создаем и настраиваем диалог
+        val dialog = builder.create()
+        dialog.setTitle("City name:")
+
+        dialog.setButton(AlertDialog.BUTTON_POSITIVE, "OK") { _, _ ->
+            listener.onClick(editText.text.toString())
             dialog.dismiss()
         }
         dialog.setButton(AlertDialog.BUTTON_NEGATIVE, "Cancel") { _, _ ->
@@ -25,6 +47,6 @@ object DialogManager {
 
     //Своеобразный мост между диалогом и MainFragment
     interface Listener {
-        fun onClick()
+        fun onClick(name: String?)
     }
 }
